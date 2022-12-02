3034eaaee883ad2cee876f1e99560662f529511d
Ivona Milankovic <ivonamilankovic@gmail.com>
2022-12-02 11:08:46 +0100

    opening menu

--- app/src/main/java/com/example/words/LeaderBoard.java
+++ app/src/main/java/com/example/words/LeaderBoard.java
@@ -2,7 +2,9 @@ package com.example.words;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LeaderBoard extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LeaderBoard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
+    }
+
+
+    public void openMenu(View view) {
+        Intent i = new Intent(LeaderBoard.this, MainActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/words/MainActivity.java
+++ app/src/main/java/com/example/words/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.words;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout settingsLayout;
+    ImageView settingsImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        settingsLayout = findViewById(R.id.settingsMenu);
+        settingsImage = findViewById(R.id.settingsImage);
+    }
+
+    public void openSettings(View view) {
+        if(settingsLayout.getVisibility() == View.INVISIBLE){
+            settingsLayout.setVisibility(View.VISIBLE);
+            settingsImage.setImageResource(R.drawable.up);
+        }else{
+            settingsLayout.setVisibility(View.INVISIBLE);
+            settingsImage.setImageResource(R.drawable.down);
+        }
+    }
+
+    public void exitMenu(View view) {
+        Intent i = new Intent(MainActivity.this, LeaderBoard.class);
+        startActivity(i);
     }
 }
