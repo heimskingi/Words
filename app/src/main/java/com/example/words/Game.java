c8a7759b9389aa6121db3f14a11f63f6c392d08f
Ivona Milankovic <ivonamilankovic@gmail.com>
2022-12-23 09:04:11 +0100

    list adapter for letters

--- app/src/main/java/com/example/words/Game.java
+++ app/src/main/java/com/example/words/Game.java
@@ -5,16 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Random;
 
 public class Game extends AppCompatActivity {
 
     TextView wordTextView, engTextView;
+    ListView lettersListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +23,7 @@ public class Game extends AppCompatActivity {
         setContentView(R.layout.activity_game);
         wordTextView = findViewById(R.id.word);
         engTextView = findViewById(R.id.wordEng);
+        lettersListView = findViewById(R.id.listViewLetters);
 
         int level = getIntent().getExtras().getInt("level", 1);
         Database db = new Database(Game.this);
@@ -37,29 +39,32 @@ public class Game extends AppCompatActivity {
         Random random = new Random();
         Word word = words.get(random.nextInt(words.size()));
         wordTextView.setText(word.getSerbianWord());
+        //num of lines is num of letters of eng word
         String lines = "";
         for(int y=0; y<word.englishWord.length();y++){
             lines+="_";
         }
         engTextView.setText(lines);
+        //list of chars needed for completing the word +  additional chars based on level
+        ArrayList<Character> characters = new ArrayList<>();
         char[] neededLetters = word.getEnglishWord().toUpperCase().toCharArray();
+        for(int i= 0 ; i < neededLetters.length; i++){
+            characters.add(neededLetters[i]);
+        }
         int numOfAddChars = 0;
         if (level == 2) {
             numOfAddChars = 3;
         } else if (level == 3) {
             numOfAddChars = 5;
         }
-        char[] additionalLetters = new char[numOfAddChars];
         for (int x = 0; x < numOfAddChars; x++) {
-            additionalLetters[x] = chars[random.nextInt(chars.length)];
+            char ch = chars[random.nextInt(chars.length)];
+            characters.add(ch);
         }
-        StringBuilder sb = new StringBuilder();
-        sb.append(neededLetters);
-        sb.append(additionalLetters);
-        char[] allChars = sb.toString().toCharArray();
 
-        //listadapter za slova
-
+        //listAdapter to show chars
+        GameLetterListAdapter listAdapter = new GameLetterListAdapter(Game.this, characters);
+        lettersListView.setAdapter(listAdapter);
 
 
     }
